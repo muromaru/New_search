ec347eed8210dafdbd7c0ccabb53c0c29b8d2608
muromaru <fnd.kt.nnst@gmail.com>
2021-08-27 17:08:13 +0900

    aaa

---
+++ New_search/app/src/main/java/muroga/matsuoka/new_search/murogaActivity.java
@@ -0,0 +1,17 @@
+package muroga.matsuoka.new_search;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class murogaActivity extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+
+
+
+}
