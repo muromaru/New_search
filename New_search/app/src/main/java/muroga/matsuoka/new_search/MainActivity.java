aa55de35dd0de1396d84290ab60ee159300f84ea
松岡宏樹 <matsuokahiroki@matsuokahirokinoMacBook-Air.local>
2021-09-04 16:02:28 +0900

    H210904_4_Mainのimage1をimagebuttumに変更し連携させる

--- New_search/app/src/main/java/muroga/matsuoka/new_search/MainActivity.java
+++ New_search/app/src/main/java/muroga/matsuoka/new_search/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textview);
         final ImageView imageView0 = findViewById(R.id.imageview0);
-        final ImageView imageView1 = findViewById(R.id.imageview1);
+//        final ImageView imageView1 = findViewById(R.id.imageview1);
         final ImageView imageView2 = findViewById(R.id.imageview2);
         final ImageView imageView3 = findViewById(R.id.imageview3);
         final ImageView imageView4 = findViewById(R.id.imageview4);
@@ -50,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
         Button button3 = findViewById(R.id.button3);
         Button button4 = findViewById(R.id.button4);
         Button button5 = findViewById(R.id.button5);
-        Button button6 = findViewById(R.id.button6);
 
 
         button0.setOnClickListener(new View.OnClickListener() {
@@ -84,7 +83,19 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("abc", "Exe1");
                 String str = String.join("\n", textValues[1]);
                 textView.setText(str);
-                imageView1.setImageBitmap(imageValues[1]);
+                ImageButton imgbutton1 = findViewById(R.id.imagebutton1);
+                imgbutton1.setImageBitmap(imageValues[1]);
+                imgbutton1.setOnClickListener(new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void onClick(View v) {
+                        Log.d("abc", "imagebutton1");
+                        //MainActivity2.setImageValues2(imageValues[0]);
+                        Intent intent = new Intent(getApplication(), MatsuokaActivity1.class);
+//                        intent.putExtra("TEst", "Sample Message");
+                        startActivity(intent);
+                    }
+                });
 
             }
         });
@@ -134,16 +145,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("abc", "aaaaaaaaaaaaaaaaa");
                 Intent intent = new Intent(MainActivity.this, MurogaActivity2.class);
                 intent.putExtra("TEst", "Sample Message");
-                startActivity(intent);
-            }
-        });
-        button6.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void onClick(View v) {
-                Log.d("abc", "Exe6");
-                Intent intent = new Intent(getApplication(), MatsuokaActivity1.class);
-//                intent.putExtra("TEst", "Sample Message");
                 startActivity(intent);
             }
         });
