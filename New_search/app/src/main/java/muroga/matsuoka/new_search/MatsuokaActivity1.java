095a46fd4347fdec350563ea58d6e94a85022764
松岡宏樹 <matsuokahiroki@matsuokahirokinoMacBook-Air.local>
2021-09-06 23:59:33 +0900

    H210906_2_縦スクロールも追加

--- New_search/app/src/main/java/muroga/matsuoka/new_search/MatsuokaActivity1.java
+++ New_search/app/src/main/java/muroga/matsuoka/new_search/MatsuokaActivity1.java
@@ -32,6 +32,18 @@ public class MatsuokaActivity1 extends AppCompatActivity {
         ImageView image3 = findViewById(R.id.imageview_3);
         image3.setImageBitmap(imageValues2[3]);
 
+        ImageView image4 = findViewById(R.id.imageview_4);
+        image4.setImageBitmap(imageValues2[4]);
+
+        ImageView image5 = findViewById(R.id.imageview_5);
+        image5.setImageBitmap(imageValues2[5]);
+
+        ImageView image6 = findViewById(R.id.imageview_6);
+        image6.setImageBitmap(imageValues2[6]);
+
+        ImageView image7 = findViewById(R.id.imageview_7);
+        image7.setImageBitmap(imageValues2[7]);
+
 
     }
 
